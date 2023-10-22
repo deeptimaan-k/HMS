c3e81e9fcbf26cdb853d84b0e071aebbd893ccd8
PyProdger <deeptimaankrishnajadaun@gmail.com>
2023-10-22 14:14:22 +0530

    Update DLogin.jsx

--- src/Pages/Dashboard/Dashboard-Login/DLogin.jsx
+++ src/Pages/Dashboard/Dashboard-Login/DLogin.jsx
@@ -1,48 +1,61 @@
 import React, { useState } from "react";
-import { Radio } from "antd";
+import { Radio, Button, Spin } from "antd"; 
 import banner from "../../../img/banner.png";
 import admin from "../../../img/admin.jpg";
-import { useNavigate } from "react-router-dom";
 import axios from "axios"; // Import Axios
+import { useNavigate } from "react-router-dom";
 import "./DLogin.css";
 
 const DLogin = () => {
   const [userType, setUserType] = useState("Hospital");
-  const navigate = useNavigate();
-
+  const [userID, setUserID] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate(); 
+  const [loading, setLoading] = useState(false);
   const handleUserTypeChange = (e) => {
     setUserType(e.target.value);
   };
 
-  const handleLoginSubmit = async () => {
+  const handleLoginSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
     try {
       // Define the user data for login
       const userData = {
         userType,
-        userID: document.querySelector("[name=ID]").value,
-        password: document.querySelector("[name=password]").value,
+        userID,
+        password,
       };
-
+      console.log("Submitting:", userData);
       // Send a POST request to your backend's login endpoint
       const response = await axios.post(
-        "https://long-tan-crane-gear.cyclic.app/api/auth/login",
+        "https://long-tan-crane-gear.cyclic.app/api/auth/login", // Update with your server URL
         userData
       );
 
       if (response.data.success) {
         // Redirect the user to the appropriate page
-        if (userType === "hospital") {
+        if (userType === "Hospital") {
+          console.log("Hospital login successful");
           navigate("/hospital");
         } else if (userType === "Patient") {
+          console.log("Patient login successful");
           navigate("/patient");
         } else {
+          console.log("Insurance login successful");
           navigate("/insurance");
         }
       } else {
         // Handle login failure (show an error message, for example)
+        console.log("Login failed");
+        // Implement error handling, like displaying an error message to the user
       }
     } catch (error) {
       console.error("Login error:", error);
+      // Implement error handling for network or other errors
+    }
+    finally{
+      setLoading(false);
     }
   };
 
@@ -70,19 +83,33 @@ const DLogin = () => {
           <img src={admin} alt="profile" />
         </div>
         <div>
-          <form>
+          <form onSubmit={handleLoginSubmit}>
             <h3>{userType === "Hospital" ? "Hospital ID" : userType === "Patient" ? "Patient ID" : "Insurance ID"}</h3>
-            <input type="number" name="ID" required />
+            <input
+              type="text"
+              value={userID}
+              onChange={(e) => setUserID(e.target.value)}
+              name="ID"
+              required
+            />
             <h3>Password</h3>
-            <input type="password" name="password" required />
-            <button type="button" onClick={handleLoginSubmit}>
-              Submit
-            </button>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+           {loading ? ( // Show a loading spinner when loading is true
+              <Spin size="large" />
+            ) : (
+              <Button type="primary" htmlType="submit">
+                {loading ? "Loading..." : "Submit"}
+              </Button>
+            )}
           </form>
         </div>
       </div>
     </div>
   );
 };
-
 export default DLogin;
