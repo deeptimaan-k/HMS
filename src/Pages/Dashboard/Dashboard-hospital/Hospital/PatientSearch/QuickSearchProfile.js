18d41e0a2a843fceb836a525df988f0b2cd8de25
PyProdger <deeptimaankrishnajadaun@gmail.com>
2023-10-25 11:41:18 +0530

    package.lock.json

--- src/Pages/Dashboard/Dashboard-hospital/Hospital/PatientSearch/QuickSearchProfile.js
+++ src/Pages/Dashboard/Dashboard-hospital/Hospital/PatientSearch/QuickSearchProfile.js
@@ -1,86 +1,41 @@
-import React from "react";
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import Nav from "../../Navigation";
 import "../../CommonCSS.css";
-import { makeStyles } from "@mui/styles";
-import TextField from "@mui/material/TextField";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-  paper: {
-    padding: theme.spacing(2),
-  },
-}));
 
 const PatientProfileForm = () => {
-  const classes = useStyles();
-
   return (
     <>
       <Nav />
-      <div className="container">
-        <div className="AfterSideBar">
-          <div className={classes.root}>
-            <Paper className={classes.paper}>
-              <Typography variant="h5" component="h2">
-                Patient Profile
-              </Typography>
-              <Grid container spacing={3}>
-                <Grid item xs={6}>
-                  <TextField label="User Type" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="User ID" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="Password" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="Patient Name" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="Mobile" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="Email" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="Age" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="Department" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="Gender" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="Blood Group" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="DOB" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="Address" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="Disease" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="Details" fullWidth />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField label="Admitted Date" fullWidth />
-                </Grid>
-              </Grid>
-            </Paper>
-          </div>
-        </div>
+      <h1 style={{ color: "rgb(184 191 234)" }}>Patient Profile: </h1>
+      <form>
+      <div>
+        <TextField
+          label="Name"
+          name="name"
+          fullWidth
+        />
       </div>
+      <div>
+        <TextField
+          label="Age"
+          name="age"
+          fullWidth
+        />
+      </div>
+      <div>
+        <TextField
+          label="Date of Birth"
+          name="dob"
+          
+          fullWidth
+        />
+      </div>
+      <Button type="submit" variant="contained" color="primary">
+        Submit
+      </Button>
+    </form>
     </>
   );
 };
