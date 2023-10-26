cfd9d559ca4ef1953a297f3f0ec4907dcc23b127
PyProdger <deeptimaankrishnajadaun@gmail.com>
2023-10-26 10:56:25 +0530

    Update QuickSearchProfile.js

--- src/Pages/Dashboard/Dashboard-hospital/Hospital/PatientSearch/QuickSearchProfile.js
+++ src/Pages/Dashboard/Dashboard-hospital/Hospital/PatientSearch/QuickSearchProfile.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Button from '@mui/material/Button';
 import Nav from "../../Navigation";
 import "../../CommonCSS.css";
@@ -10,31 +12,34 @@ const PatientProfileForm = () => {
       <Nav />
       <h1 style={{ color: "rgb(184 191 234)" }}>Patient Profile: </h1>
       <form>
-      <div>
+      <Container className='mt-20'>
+      <Grid container spacing={2}>
         <TextField
           label="Name"
           name="name"
           fullWidth
         />
-      </div>
-      <div>
+      </Grid>
+      <Grid container spacing={2}>
         <TextField
           label="Age"
           name="age"
           fullWidth
         />
-      </div>
-      <div>
+      </Grid>
+      <Grid container spacing={2}>
         <TextField
           label="Date of Birth"
           name="dob"
-          
           fullWidth
         />
-      </div>
+      </Grid>
+      <Grid container spacing={2}>
       <Button type="submit" variant="contained" color="primary">
         Submit
       </Button>
+      </Grid>
+      </Container>
     </form>
     </>
   );
